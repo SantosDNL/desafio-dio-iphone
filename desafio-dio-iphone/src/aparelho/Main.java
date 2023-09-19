6dc6bbad5ded6798d1a77853e085da44e7e037ee
SantosDNL <ds3558723@gmail.com>
2023-09-19 14:42:26 -0300

    first commit

---
+++ desafio-dio-iphone/src/aparelho/Main.java
@@ -0,0 +1,27 @@
+package aparelho;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    AparelhoMultiuso aparelho = new AparelhoMultiuso();
+
+    System.out.println();
+    aparelho.selecionarMusica("Música Teste");
+    aparelho.tocar();
+    aparelho.pausar();
+
+    System.out.println();
+    aparelho.atender();
+    aparelho.iniciarCorreioVoz();
+    aparelho.ligar(12345678);
+
+    System.out.println();
+    aparelho.exibirPagina();
+    aparelho.atualizarPagina();
+    aparelho.adicionarNovaAba();
+
+    System.out.println();
+  }
+
+}
